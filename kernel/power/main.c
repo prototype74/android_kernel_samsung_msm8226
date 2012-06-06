010ed6d49186bb9a182f1def8740b79cadb14dad
Arve Hjønnevåg <arve@android.com>
2012-06-05 18:06:25 -0700

    Revert "PM: Add user-space wake lock api."
    This reverts commit 025ff0ad2ff6030b7071fc3da7dda4a96ec91468.

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -969,6 +969,7 @@ power_attr(pm_trace_dev_match);
 
 #endif /* CONFIG_PM_TRACE */
 
+<<<<<<< HEAD
 #ifdef CONFIG_USER_WAKELOCK
 power_attr(wake_lock);
 power_attr(wake_unlock);
@@ -1184,6 +1185,9 @@ power_attr(cpufreq_min_limit);
 power_attr(cpufreq_table);
 #endif
 static struct attribute *g[] = {
+=======
+static struct attribute * g[] = {
+>>>>>>> 3f04bae581c... Revert "PM: Add user-space wake lock api."
 	&state_attr.attr,
 #ifdef CONFIG_PM_TRACE
 	&pm_trace_attr.attr,
@@ -1203,10 +1207,6 @@ static struct attribute *g[] = {
 	&touch_event_timer_attr.attr,
 #ifdef CONFIG_PM_DEBUG
 	&pm_test_attr.attr,
-#endif
-#ifdef CONFIG_USER_WAKELOCK
-	&wake_lock_attr.attr,
-	&wake_unlock_attr.attr,
 #endif
 #endif
 #ifdef CONFIG_CPU_FREQ_LIMIT_USERSPACE
