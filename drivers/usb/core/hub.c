5ef6e3ce218b906a2d2162bb4baa1dd22377997a
Theodore Ts'o <tytso@mit.edu>
2012-07-04 11:22:20 -0400

    usb: feed USB device information to the /dev/random driver
    commit b04b3156a20d395a7faa8eed98698d1e17a36000 upstream.
    
    Send the USB device's serial, product, and manufacturer strings to the
    /dev/random driver to help seed its pools.
    
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Acked-by: Greg KH <greg@kroah.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -24,7 +24,11 @@
 #include <linux/kthread.h>
 #include <linux/mutex.h>
 #include <linux/freezer.h>
+<<<<<<< HEAD
 #include <linux/usb/otg.h>
+=======
+#include <linux/random.h>
+>>>>>>> 76b9be5d7fe... usb: feed USB device information to the /dev/random driver
 
 #include <asm/uaccess.h>
 #include <asm/byteorder.h>
@@ -2053,6 +2057,14 @@ int usb_new_device(struct usb_device *udev)
 #endif
 	call_battery_notify(udev, 1);
 #endif
+	if (udev->serial)
+		add_device_randomness(udev->serial, strlen(udev->serial));
+	if (udev->product)
+		add_device_randomness(udev->product, strlen(udev->product));
+	if (udev->manufacturer)
+		add_device_randomness(udev->manufacturer,
+				      strlen(udev->manufacturer));
+
 	device_enable_async_suspend(&udev->dev);
 
 	/*
