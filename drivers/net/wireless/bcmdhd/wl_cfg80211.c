b9e1e03e22dda40e780974010bab779f6a3ceb44
Dmitry Shmidt <dimitrysh@google.com>
2012-08-20 11:19:20 -0700

    net: wireless: bcmdhd: Update to Version 1.28.9-2
    Change-Id: Ifd55ef8645a7cf613bcbd1f5ea3a71fe249a96ac
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -8073,14 +8073,6 @@ static s32 wl_inform_single_bss(struct bcm_cfg80211 *cfg, struct wl_bss_info *bi
 	}
 
 
-	if (!mgmt->u.probe_resp.timestamp) {
-		struct timespec ts;
-
-		get_monotonic_boottime(&ts);
-		mgmt->u.probe_resp.timestamp = ((u64)ts.tv_sec * 1000000)
-						+ ts.tv_nsec / 1000;
-	}
-
 	cbss = cfg80211_inform_bss_frame(wiphy, channel, mgmt,
 		le16_to_cpu(notif_bss_info->frame_len), signal, aflags);
 	if (unlikely(!cbss)) {
