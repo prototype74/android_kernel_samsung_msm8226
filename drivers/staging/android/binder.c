3d4803db5e50c2d62d53fd2d46b1e35ae55337e2
Arve Hjønnevåg <arve@android.com>
2014-02-13 20:25:06 -0800

    Staging: android: binder: More offset validation.
    Make sure offsets don't point to overlapping flat_binder_object
    structs.
    
    Change-Id: I425ab0c46fbe2b00ed679c5becf9e8140395eb40
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Git-commit: 457c3cd05958b8397211ae1f6dd3c3d325f4c0ea
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1410,6 +1410,7 @@ static void binder_transaction(struct binder_proc *proc,
 	struct binder_transaction *t;
 	struct binder_work *tcomplete;
 	binder_size_t *offp, *off_end;
+	binder_size_t off_min;
 	struct binder_proc *target_proc;
 	struct binder_thread *target_thread = NULL;
 	struct binder_node *target_node = NULL;
