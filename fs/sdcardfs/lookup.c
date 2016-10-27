8f5a084e19faa8264ea59871c68e3232ecef884d
Daniel Rosenberg <drosen@google.com>
2016-10-26 20:27:20 -0700

    sdcardfs: Use per mount permissions
    This switches sdcardfs over to using permission2.
    Instead of mounting several sdcardfs instances onto
    the same underlaying directory, you bind mount a
    single mount several times, and remount with the
    options you want. These are stored in the private
    mount data, allowing you to maintain the same tree,
    but have different permissions for different mount
    points.
    
    Warning functions have been added for permission,
    as it should never be called, and the correct
    behavior is unclear.
    
    Change-Id: I841b1d70ec60cf2b866fa48edeb74a0b0f8334f5
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- fs/sdcardfs/lookup.c
+++ fs/sdcardfs/lookup.c
@@ -240,6 +240,30 @@ static struct dentry *__sdcardfs_lookup(struct dentry *dentry,
 	/* Use vfs_path_lookup to check if the dentry exists or not */
 	err = vfs_path_lookup(lower_dir_dentry, lower_dir_mnt, name, 0,
 				&lower_nd.path);
+	/* check for other cases */
+	if (err == -ENOENT) {
+		struct dentry *child;
+		struct dentry *match = NULL;
+		mutex_lock(&lower_dir_dentry->d_inode->i_mutex);
+		spin_lock(&lower_dir_dentry->d_lock);
+		list_for_each_entry(child, &lower_dir_dentry->d_subdirs, d_child) {
+			if (child && child->d_inode) {
+				if (strcasecmp(child->d_name.name, name)==0) {
+					match = dget(child);
+					break;
+				}
+			}
+		}
+		spin_unlock(&lower_dir_dentry->d_lock);
+		mutex_unlock(&lower_dir_dentry->d_inode->i_mutex);
+		if (match) {
+			err = vfs_path_lookup(lower_dir_dentry,
+						lower_dir_mnt,
+						match->d_name.name, 0,
+						&lower_nd.path);
+			dput(match);
+		}
+	}
 
 	/* no error: handle positive dentries */
 	if (!err) {
@@ -370,7 +394,7 @@ struct dentry *sdcardfs_lookup(struct inode *dir, struct dentry *dentry,
 					sdcardfs_lower_inode(dentry->d_inode));
 		/* get derived permission */
 		get_derived_permission(parent, dentry);
-		fix_derived_permission(dentry->d_inode);
+		fixup_tmp_permissions(dentry->d_inode);
 	}
 	/* update parent directory's atime */
 	fsstack_copy_attr_atime(parent->d_inode,
