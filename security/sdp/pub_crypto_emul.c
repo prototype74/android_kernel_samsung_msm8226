335401fc4340b510fc057fc45bbbcf18c7c87210
prototype74 <46874884+prototype74@users.noreply.github.com>
2022-05-03 22:10:56 +0200

    sdp: set expected kernel version > 3.4.113

--- security/sdp/pub_crypto_emul.c
+++ security/sdp/pub_crypto_emul.c
@@ -471,7 +471,7 @@ void pub_crypto_control_init(pub_crypto_control_t *con) {
 }
 
 static int __init pub_crypto_mod_init(void) {
-#if (LINUX_VERSION_CODE > KERNEL_VERSION(3,4,67))
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(3,4,113))
 	struct netlink_kernel_cfg cfg = {
 		.input  = crypto_recver,
 	};
